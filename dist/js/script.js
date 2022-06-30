2cea94b76c827d092c45a424cd6364e9d7484779
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-06-30 18:08:42 +0600

    Create rating

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const ratingNums = Array.from(document.querySelectorAll('.rating__item-num'));    
+    const ratingProgress = Array.from(document.querySelectorAll('.rating__item-progress'));   
+    
+    ratingProgress.forEach((item, i) => {
+        item.style.width = ratingNums[i].innerText;
+    })
+})
